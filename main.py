b78b95cf8af96e48ee9c3aa84b32edcc174cc9bf
liaoke123 <104013620+liaoke01@users.noreply.github.com>
2024-03-12 18:29:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from telegram import Bot, Update, ParseMode
-from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filtersfrom telegram import Bot, Update, ParseMode
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # 替换为你的机器人API token
 TOKEN = 'YOUR_BOT_TOKEN'
